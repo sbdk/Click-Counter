67e3dcc03e03a10f6c8117ca785fea55d3caaaad
Li Yin <yinli1986@gmail.com>
2015-10-15 14:59:25 -0700

    Add initial "Click Counter" project

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Li Yin on 10/15/15.
+//  Copyright © 2015 Li Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var count = 0
+    var label:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        
+        // add Button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+    
+    }
+    
+    
+    func incrementCount(){
+        self.count++
+        self.label.text = "\(self.count)"
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
